4c678f0c199130fc47fa7441e717a1a30ebc5e21
zejnep1986 <celikzejnep@gmail.com>
2023-11-13 16:31:05 +0100

    first

---
+++ js/info.js
@@ -0,0 +1,14 @@
+function image1(){
+document.getElementById("imgid").src="image/1.jpg";
+
+}
+
+function image2(){
+
+    document.getElementById("imgid").src="image/2.jpg";
+}
+
+function image3(){
+
+    document.getElementById("imgid").src="image/3.jpg";
+}
